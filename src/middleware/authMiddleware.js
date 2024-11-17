a0a628aab7f264a00ffe471570fa237e4583391e
Aulia A Aziz <auliaanugrahaziz@gmail.com>
2024-11-17 09:03:47 +0700

    feat: add auth middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,1 +1,20 @@
-// init
+const { validateJSONToken } = require('../util/auth.js');
+require('dotenv').config();
+
+const authMiddleware = () => (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(403).send('Token is required');
+  }
+
+  const authToken = authorization.split(' ')[1];
+  try {
+    const decoded = validateJSONToken(authToken);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).send('Invalid token');
+  }
+};
+
+module.exports = authMiddleware;
